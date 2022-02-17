41056d53ffd2fff0370df0d631d2b188250dbea2
汪帅 <wangshuai1@soyoung.com>
2022-02-17 10:09:00 +0800

    golang-ffmpeg

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,28 @@
-FROM golang:1.6
-ADD . /go/src/qiniu.com/ufop/example/demo
-RUN go install qiniu.com/ufop/example/demo
-ENTRYPOINT /go/bin/demo
+FROM jrottenberg/ffmpeg:4.1-alpine AS base
+
+COPY --from=golang:1.15.5-alpine /usr/local/go/ /usr/local/go/
+
+ENV PATH="/usr/local/go/bin:${PATH}"
+
+RUN apk update && apk add build-base pkgconfig
+
+WORKDIR /src
+
+COPY go.* ./
+
+FROM base AS build
+
+COPY . .
+
+RUN go build -o /sy-ffmpeg-api server.go
+
+RUN chmod +x /sy-ffmpeg-api
+
+FROM jrottenberg/ffmpeg:4.1-alpine AS run
+
+COPY --from=build /sy-ffmpeg-api .
+
+
+ENTRYPOINT [ "/usr/bin/env" ]
+
+CMD [ "/sy-ffmpeg-api", "-env" ]
--- server.go
+++ server.go
@@ -1,76 +1,128 @@
 package main
 
 import (
-	"bufio"
+	"bytes"
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 	"os"
-	"text/template"
-	"time"
+	"os/exec"
+	"regexp"
+	"strconv"
 )
 
-// HTTPGetMaxSize 最大处理的文件长度
-const HTTPGetMaxSize = 2 * 1024 * 1024
+type Ret struct {
+	Code int    `json:"code"`
+	Msg  string `json:"msg"`
+	Data interface{} `json:"data"`
+}
 
-func httpGet(url string) (body []byte, err error) {
-	res, err := http.Get(url)
+func getTheMuteTime(url string) (muteTime float64, integratedLoudness float64, err error) {
+	cmdArguments := []string{"-i",url, "-filter_complex", "ebur128", "-c:v", "copy", "-t","10", "-f", "null", "/dev/null"}
+	command := exec.Command("ffmpeg", cmdArguments...)
+	var out bytes.Buffer
+	var errOut bytes.Buffer
+	command.Stdout = &out
+	command.Stderr = &errOut
+
+	err = command.Start()
+
 	if err != nil {
-		return nil, fmt.Errorf("httpGet error: %s", err)
+		fmt.Println(err)
 	}
-	defer res.Body.Close()
-	body, err = ioutil.ReadAll(http.MaxBytesReader(nil, res.Body, HTTPGetMaxSize))
-	if err != nil {
-		return nil, fmt.Errorf("httpGet read body error: %s", err)
+
+	if err = command.Wait(); err != nil {
+		fmt.Println(err.Error())
 	}
-	return
+
+	result := errOut.String()
+
+	reg1 := regexp.MustCompile(`t:[\s]*(\d+[\.\d+]*)\s+TARGET:(-\d+)\sLUFS\s+M:([-\s]*\d+\.\d)\s+S:([-\s]*\d+\.\d)\s+I:\s+([-\s]*\d+\.\d)\sLUFS\s+LRA:\s+(\d+\.\d)\sLU`)
+	if reg1 == nil { //解释失败，返回nil
+		fmt.Println("regexp err")
+		return
+	}
+
+	result1 := reg1.FindAllStringSubmatch(result, -1)
+
+	// fmt.Println("result1 = ", result1)
+
+	if len(result1) == 0 {
+		fmt.Println("result1 is empty")
+		return
+	}
+
+	for _, v := range result1 {
+		time, _ := strconv.ParseFloat(v[1], 10)
+		volume, _ := strconv.ParseFloat(v[5], 10)
+
+		//fmt.Println(time, volume)
+
+		if volume > -70.0 {
+			muteTime = time
+			break
+		}
+	}
+
+	reg2 := regexp.MustCompile(`\s+I:\s+([-+\s]\d+\.\d) LUFS`)
+	if reg2 == nil { //解释失败，返回nil
+		fmt.Println("regexp err")
+		return
+	}
+
+	result2 := reg2.FindAllStringSubmatch(result, -1)
+
+	loudness := result2[len(result2)-1][1]
+
+	integratedLoudness, _ = strconv.ParseFloat(loudness, 10)
+	return muteTime, integratedLoudness, nil
 }
 
 func handler(rw http.ResponseWriter, req *http.Request) {
 	var err error
+
+	rw.Header().Set("Content-Type", "application/json")
+
+
 	defer func() {
 		if err != nil {
-			http.Error(rw, err.Error(), 500)
+			ret := new(Ret)
+			ret.Code = 500
+			ret.Msg = err.Error()
+			ret_json, _ := json.Marshal(ret)
+			io.WriteString(rw, string(ret_json))
 		}
 	}()
 
 	defer req.Body.Close()
 
-	var body []byte
-
 	url := req.URL.Query().Get("url")
-	if url != "" {
-		body, err = httpGet(url)
-		if err != nil {
-			log.Println("handler http get error:", err.Error())
-		}
-	} else {
-		body, err = ioutil.ReadAll(req.Body)
-		if err != nil {
-			log.Printf("handler body read error: %s\n", err.Error())
-			return
-		}
+
+	var muteTime float64
+	var integratedLoudness float64
+
+	ret := new(Ret)
+	if url == "" {
+		ret.Code = 500
+		ret.Msg = "error"
+		ret.Data = "url is empty"
 	}
 
-	tpl, err := template.New("res").Parse(`
-req body:
--------------
-{{.body}}
+	muteTime, integratedLoudness, err = getTheMuteTime(url)
 
-time: {{.time}}
-		`)
-	if err != nil {
-		panic("")
+	fmt.Println("loudness = ", integratedLoudness)
+
+	fmt.Println("muteTime = ", muteTime)
+
+	if err == nil {
+		ret.Code = 0
+		ret.Msg = "success"
+		ret.Data = map[string]float64{"mute_time": muteTime, "integrated_loudness": integratedLoudness}
+		ret_json, _ := json.Marshal(ret)
+		io.WriteString(rw, string(ret_json))
 	}
-	brw := bufio.NewWriter(rw)
-	length := len(body)
-	tpl.Execute(brw, map[string]interface{}{
-		"body":   string(body),
-		"length": length,
-		"time":   time.Now(),
-	})
-	brw.Flush()
 }
 
 func health(rw http.ResponseWriter, req *http.Request) {
@@ -82,7 +134,9 @@ func main() {
 	if port == "" {
 		port = "9100"
 	}
+
 	http.HandleFunc("/handler", handler)
 	http.HandleFunc("/health", health)
-	log.Fatalln(http.ListenAndServe("0.0.0.0:"+port, nil))
+	log.Fatalln(http.ListenAndServe("0.0.0.0:" + port, nil))
+
 }
